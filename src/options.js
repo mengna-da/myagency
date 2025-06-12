47e437b5a016a75b6c19ac98e0e3eef2a5263663
lunar-luna-dmn <md3508@nyu.edu>
2025-06-12 13:22:21 +0800

    added 3js scene

--- src/options.js
+++ src/options.js
@@ -1,22 +1,20 @@
 export const options = [
-    "Pose",
-    "Sit Down",
-    // "Lie Down",
-    "Smile",
     "Spin",
     "Jump",
-    "Walk left",
-    "Walk right",
-    "Dissapear",
     "Wave",
-    "Drink Coffee",
-    "Punch",
-    "Fold a Paper Plane",
-    "Play the Puzzle",
-    "Put on Lipstick",
-    // "Bubble wrap",
-    // "Mess Up Hair",
-    // "Comb Hair"
+    "Fight",
+    "Fly",
+    "Wave",
+    "Reach out",
+    "Zombie walk",
+    "Look",
+    "Catwalk",
+    "Bow",
+    "Clap",
+    "Pray",
+    "Shoot",
+    "Point",
+    "Play the Puzzle"
 ];
 
 export const buttonsPerSet = 10; 
